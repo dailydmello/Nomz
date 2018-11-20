8f5523bf55d4f6fba755b432a20604e5cd38b1e5
dailydmello <dmelloe@mcmaster.ca>
2018-11-20 17:24:09 -0500

    Implemented swiping action

--- Nomz/Nomz/Models/FoodCard.swift
+++ Nomz/Nomz/Models/FoodCard.swift
@@ -15,6 +15,7 @@ protocol FoodCardDelegate: NSObjectProtocol {
 }
 
 class FoodCard: UIView {
+    var xFromCenter: CGFloat = 0.0
     var xCenter: CGFloat = 0.0
     var yCenter: CGFloat = 0.0
     var imageViewStatus = UIImageView()
@@ -45,11 +46,11 @@ class FoodCard: UIView {
         addGestureRecognizer(panGestureRecognizer)
         
         //create UIImageview for food pictures
-        let backGroundImageView = UIImageView(frame: bounds)
-        backGroundImageView.image = UIImage(named: String(Int(1+arc4random()%(8-1))))
-        backGroundImageView.contentMode = .scaleAspectFill
-        backGroundImageView.clipsToBounds = true
-        addSubview(backGroundImageView)
+        let foodImageView = UIImageView(frame: bounds)
+        foodImageView.image = UIImage(named: String(Int(1+arc4random()%(8-1))))
+        foodImageView.contentMode = .scaleAspectFill
+        foodImageView.clipsToBounds = true
+        addSubview(foodImageView)
         
         //create thumbs up/down uiimage
         imageViewStatus = UIImageView(frame: CGRect(x: (frame.size.width / 2) - 37.5, y: 25 , width: 75, height: 75))
@@ -57,14 +58,24 @@ class FoodCard: UIView {
         //so its not visible in initial spot
         imageViewStatus.alpha = 0
         addSubview(imageViewStatus)
-        
-
     }
     
+    //If you have a func to selector a private func in swift
     @objc func beingDragged(_ gestureRecognizer: UIPanGestureRecognizer) {
         
+        //let foodCard = gestureRecognizer.view!
         xCenter = gestureRecognizer.translation(in: self).x
         yCenter = gestureRecognizer.translation(in: self).y
+        
+        let foodCard = gestureRecognizer.view!
+        let point = gestureRecognizer.translation(in: self)
+//        let xFromCenter = foodCard.center.x - self.center.x
+//        foodCard.center = CGPoint(x: self.center.x + point.x, y: self.center.y + point.y)
+//
+//        let scale = min(100/abs(xFromCenter),1)
+//        foodCard.transform = CGAffineTransform(rotationAngle: (2 * 0.61 * xFromCenter)/self.frame.width).scaledBy(x: scale, y: scale)
+
+        
         switch gestureRecognizer.state {
         // Keep swiping
         case .began:
@@ -72,19 +83,18 @@ class FoodCard: UIView {
             break;
         //in the middle of a swipe
         case .changed:
-            let rotationStrength = min(xCenter / UIScreen.main.bounds.size.width, 1)
-            let rotationAngel = .pi/8 * rotationStrength
-            //let scale = max(1 - abs(rotationStrength) / SCALE_STRENGTH, SCALE_RANGE)
-            center = CGPoint(x: originalPoint.x + xCenter, y: originalPoint.y + yCenter)
-            let transforms = CGAffineTransform(rotationAngle: rotationAngel)
-            //let scaleTransform: CGAffineTransform = transforms.scaledBy(x: scale, y: scale)
-            //self.transform = scaleTransform
-            //updateOverlay(xCenter)
+            xFromCenter = foodCard.center.x - self.center.x
+            foodCard.center = CGPoint(x: self.center.x + point.x, y: self.center.y + point.y)
+            
+            let scale = min(100/abs(xFromCenter),1)
+            foodCard.transform = CGAffineTransform(rotationAngle: (2 * 0.61 * xFromCenter)/self.frame.width).scaledBy(x: scale, y: scale)
+            
+            updateOverlay(xFromCenter: xFromCenter)
             break;
             
         // swipe ended
         case .ended:
-            //afterSwipeAction()
+            afterSwipeAction(foodCard: foodCard)
             break;
             
         case .possible:break
@@ -93,5 +103,50 @@ class FoodCard: UIView {
         }
     }
     
+    func updateOverlay(xFromCenter: CGFloat){
+        //update thumbs depending on swipe direction +left, -right
+        if xFromCenter > 0 {
+            imageViewStatus.image = #imageLiteral(resourceName: "ThumbUp")
+            imageViewStatus.tintColor = UIColor.green
+        } else {
+            imageViewStatus.image = #imageLiteral(resourceName: "ThumbDown.png")
+            imageViewStatus.tintColor = UIColor.red
+        }
+        //make thumbs more visible as you move right/left
+        imageViewStatus.alpha = abs(xFromCenter)/self.center.x
+    }
     
+    func afterSwipeAction(foodCard: UIView){
+        if foodCard.center.x < 75 {
+            UIView.animate(withDuration: 0.3, animations: {
+                foodCard.center = CGPoint(x: foodCard.center.x - 200, y: foodCard.center.y + 75)
+                foodCard.alpha = 0
+            }, completion: {(_) in
+                self.removeFromSuperview()
+            })
+            isLiked = false
+            delegate?.cardGoesRight(card: self)
+            print("Going right")
+            return
+            
+        } else if foodCard.center.x > (self.frame.width - 75){
+            UIView.animate(withDuration: 0.3, animations: {
+                foodCard.center = CGPoint(x: foodCard.center.x + 200, y: foodCard.center.y + 75)
+                foodCard.alpha = 0
+            }, completion: {(_) in
+                self.removeFromSuperview()
+            })
+            isLiked = true
+            delegate?.cardGoesLeft(card: self)
+            print("Going left")
+            return
+        }
+        // return to center if not fully swiped
+        UIView.animate(withDuration: 0.2) {
+            foodCard.center = self.center
+            self.imageViewStatus.alpha = 0
+            foodCard.alpha = 1
+            foodCard.transform = CGAffineTransform.identity
+        }
+    }
 }
